f703de2bc9f1679a84146f1c9f7ac7344cc3ff76
Piotr Muzyka <piotrmuzyka@yahoo.com>
2019-01-13 18:54:58 +0100

    Initial commit.

---
+++ spring_dao/src/main/java/com/pmuz/model/User.java
@@ -0,0 +1,23 @@
+package com.pmuz.model;
+
+public class User {
+
+
+	private String username;
+	private String password;
+	
+	public User(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+	// constructors / standard setters / getters
+
+	public void setEmail(String requireNonNull) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setName(String requireNonNull) {
+		// TODO Auto-generated method stub
+		
+	}
+}
